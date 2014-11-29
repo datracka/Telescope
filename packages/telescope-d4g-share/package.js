c304c86a5cf19b11e4408f1a0f905d96127b32c1
Jeremy Shimko <jshimko@cloudlinemedia.co>
2014-11-29 14:23:02 -0500

    add custom social sharing package

---
+++ packages/telescope-d4g-share/package.js
@@ -0,0 +1,17 @@
+Package.describe({summary: "Telescope share module package for DataLook.io"});
+
+Package.onUse(function (api) {
+
+  api.use(['telescope-lib', 'telescope-base'], ['client', 'server']);
+
+  api.use([
+    'jquery',
+    'underscore',
+    'templating'
+  ], 'client');
+
+  api.add_files(['lib/share.js'], ['client', 'server']);
+
+  api.add_files(['lib/client/post_share.html', 'lib/client/post_share.js'], ['client']);
+
+});
